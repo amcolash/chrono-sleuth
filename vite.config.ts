94231758e686f844f532bab13346835cb3d3cb7b
Andrew McOlash <amcolash@gmail.com>
2024-07-20 15:04:46 -0700

    reduce favicon size, web manifest, pwa caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
+
+const manifest: Partial<ManifestOptions> = {
+  theme_color: '#b5c1b9',
+  background_color: '#3e424e',
+  display: 'fullscreen',
+  start_url: '/chrono-sleuth/',
+  id: 'com.amcolash.chrono',
+  name: 'Chrono Sleuth',
+  description: 'Chrono Sleuth is a detective game set in a mysterious town.',
+
+  icons: [
+    {
+      src: './favicon.webp',
+      sizes: 'any',
+      type: 'image/webp',
+      purpose: 'any maskable',
+    },
+  ],
+};
 
 export default defineConfig({
   base: './',
-  logLevel: 'warn',
   build: {
     rollupOptions: {
       output: {
@@ -25,5 +44,19 @@ export default defineConfig({
   define: {
     __BUILD_TIME__: new Date(),
   },
-  plugins: [],
+  plugins: [
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1000 * 1000, // 10mb
+      },
+      // cache static assets in the public folder
+      includeAssets: ['**/*'],
+      manifest,
+
+      devOptions: {
+        // Enable to have service worker/manifest in dev mode
+        // enabled: true,
+      },
+    }),
+  ],
 });
