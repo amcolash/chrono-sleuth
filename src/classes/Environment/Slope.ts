ccd395136937bb846018b510f0ebcc31542f1cb1
Andrew McOlash <amcolash@gmail.com>
2024-08-01 23:57:06 -0700

    allow ramps to be dragged in debug mode, add upwards ramps

--- src/classes/Environment/Slope.ts
+++ src/classes/Environment/Slope.ts
@@ -1,30 +1,46 @@
-import { GameObjects, Math, Physics, Scene } from 'phaser';
+import { GameObjects, Math as PhaserMath, Physics } from 'phaser';
 
 import { Config } from '../../config';
 import { Game } from '../../scenes/Game';
+import { speed } from '../Player/Player';
+import { Key } from '../UI/InputManager';
 
 export class Slope extends Physics.Arcade.Image {
+  scene: Game;
   width: number;
   height: number;
   flipped: boolean;
+  upwards: boolean;
   graphics: GameObjects.Graphics;
 
-  constructor(scene: Scene, x: number, y: number, width: number = 100, height: number = 100, flip: boolean = false) {
+  constructor(
+    scene: Game,
+    x: number,
+    y: number,
+    width: number = 100,
+    height: number = 100,
+    flip: boolean = false,
+    upwards: boolean = false
+  ) {
     super(scene, x, y, '');
+    this.scene = scene;
+
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
-    this.scene = scene;
     this.width = width;
     this.height = height;
     this.flipped = flip;
+    this.upwards = upwards;
 
     this.setOrigin(0);
 
-    this.setVisible(false);
+    if (!Config.debug) this.setVisible(false);
     this.setSize(width, height * 1.5);
 
     if (Config.debug) {
+      this.setInteractive({ draggable: true });
+
       const graphics = scene.add.graphics();
       this.graphics = graphics;
 
@@ -32,8 +48,8 @@ export class Slope extends Physics.Arcade.Image {
 
       const halfWidth = width / 2;
 
-      const left = new Math.Vector2(x, flip ? y : y + height);
-      const right = new Math.Vector2(x + width, flip ? y + height : y);
+      const left = new PhaserMath.Vector2(0, flip ? 0 : 0 + height);
+      const right = new PhaserMath.Vector2(0 + width, flip ? 0 + height : 0);
 
       graphics.lineBetween(left.x, left.y, right.x, right.y);
       graphics.lineBetween(left.x - halfWidth, left.y, left.x, left.y);
@@ -45,18 +61,39 @@ export class Slope extends Physics.Arcade.Image {
   }
 
   update(_time: number, _delta: number) {
-    const player = (this.scene as Game).player;
+    if (Config.debug && this.graphics) {
+      this.graphics.setPosition(this.x, this.y);
+    }
+
+    const player = this.scene.player;
+    const keys = player.keys.keys;
 
     if (
       this.body &&
       this.scene.physics.world.intersects(this.body as Physics.Arcade.Body, player.body as Physics.Arcade.Body)
     ) {
-      let horizontalPercent = Math.Clamp(1 - (this.x + this.width - player.x) / this.width, 0, 1);
+      let horizontalPercent = PhaserMath.Clamp(1 - (this.x + this.width - player.x) / this.width, 0, 1);
       if (this.flipped) horizontalPercent = 1 - horizontalPercent;
 
       const bottom = this.y + this.height;
       const offset = (1 - player.originY) * player.displayHeight;
       const newY = bottom - this.height * horizontalPercent - offset;
+
+      // if up key pressed and player is close to the slope, move up
+      if (this.upwards && keys[Key.Up] && Math.abs(player.y - newY) < 70) {
+        player.setVelocityX(this.flipped ? -speed : speed);
+        player.setFlipX(this.flipped);
+        player.setY(newY);
+        return;
+      }
+
+      // if moving left/right and above ground, move up
+      if (this.upwards && (keys[Key.Right] || (keys[Key.Left] && this.flipped)) && player.y < bottom - 70) {
+        player.setY(newY);
+        return;
+      }
+
+      if (this.upwards && newY < player.y) return;
 
       player.setY(newY);
     }
