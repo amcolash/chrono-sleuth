bcb42f6eb97faa9a111913ca377bb7bcbd1c0b47
Andrew McOlash <amcolash@gmail.com>
2025-01-01 02:44:03 -0800

    change some things up to make stats nicer + consistent

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -11,14 +11,14 @@ var Stats = function () {
 
   var container = document.createElement('div');
   container.style.cssText = 'position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000';
-  container.addEventListener(
-    'click',
-    function (event) {
-      event.preventDefault();
-      showPanel(++mode % container.children.length);
-    },
-    false
-  );
+  // container.addEventListener(
+  //   'click',
+  //   function (event) {
+  //     event.preventDefault();
+  //     showPanel(++mode % container.children.length);
+  //   },
+  //   false
+  // );
 
   //
 
@@ -37,18 +37,7 @@ var Stats = function () {
 
   //
 
-  var beginTime = (performance || Date).now(),
-    prevTime = beginTime,
-    frames = 0;
-
-  var fpsPanel = addPanel(new Stats.Panel('FPS', '#0ff', '#002'));
-  var msPanel = addPanel(new Stats.Panel('MS', '#0f0', '#020'));
-
-  if (self.performance && self.performance.memory) {
-    var memPanel = addPanel(new Stats.Panel('MB', '#f08', '#201'));
-  }
-
-  showPanel(0);
+  // showPanel(0);
 
   return {
     REVISION: 16,
@@ -57,36 +46,6 @@ var Stats = function () {
 
     addPanel: addPanel,
     showPanel: showPanel,
-
-    begin: function () {
-      beginTime = (performance || Date).now();
-    },
-
-    end: function () {
-      frames++;
-
-      var time = (performance || Date).now();
-
-      msPanel.update(time - beginTime, 200);
-
-      if (time >= prevTime + 1000) {
-        fpsPanel.update((frames * 1000) / (time - prevTime), 100);
-
-        prevTime = time;
-        frames = 0;
-
-        if (memPanel) {
-          var memory = performance.memory;
-          memPanel.update(memory.usedJSHeapSize / 1048576, memory.jsHeapSizeLimit / 1048576);
-        }
-      }
-
-      return time;
-    },
-
-    update: function () {
-      beginTime = this.end();
-    },
 
     // Backwards Compatibility
 
@@ -101,14 +60,14 @@ Stats.Panel = function (name, fg, bg) {
     round = Math.round;
   var PR = round(window.devicePixelRatio || 1);
 
-  var WIDTH = 80 * PR,
-    HEIGHT = 48 * PR,
+  var WIDTH = 90 * PR,
+    HEIGHT = 52 * PR,
     TEXT_X = 3 * PR,
     TEXT_Y = 2 * PR,
     GRAPH_X = 3 * PR,
-    GRAPH_Y = 15 * PR,
-    GRAPH_WIDTH = 74 * PR,
-    GRAPH_HEIGHT = 30 * PR;
+    GRAPH_Y = 23 * PR,
+    GRAPH_WIDTH = 84 * PR,
+    GRAPH_HEIGHT = 27 * PR;
 
   var canvas = document.createElement('canvas');
   canvas.width = WIDTH;
@@ -130,19 +89,29 @@ Stats.Panel = function (name, fg, bg) {
   context.globalAlpha = 0.9;
   context.fillRect(GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT);
 
+  var lastUpdate = 0;
+  var index = 0;
+  var data = [];
+
   return {
     dom: canvas,
 
     update: function (value) {
-      // change to slowly decrease the min/max values back to zero
-      min = Math.min(min * 0.99, value);
-      max = Math.max(max * 0.99, value);
+      if (performance.now() - lastUpdate < 100) return;
+
+      data[index] = value;
+      index = (index + 1) % GRAPH_WIDTH;
+
+      min = Math.min(...data);
+      max = Math.max(...data);
 
       context.fillStyle = bg;
       context.globalAlpha = 1;
       context.fillRect(0, 0, WIDTH, GRAPH_Y);
       context.fillStyle = fg;
-      context.fillText(round(value) + ' ' + name + ' (' + round(min) + '-' + round(max) + ')', TEXT_X, TEXT_Y);
+
+      context.fillText(`${name}: ${round(value)}`, TEXT_X, TEXT_Y);
+      context.fillText(`[${round(min)} - ${round(max)}]`, TEXT_X, TEXT_Y + 10);
 
       context.drawImage(
         canvas,
@@ -160,7 +129,14 @@ Stats.Panel = function (name, fg, bg) {
 
       context.fillStyle = bg;
       context.globalAlpha = 0.9;
-      context.fillRect(GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, round((1 - value / max) * GRAPH_HEIGHT));
+      context.fillRect(
+        GRAPH_X + GRAPH_WIDTH - PR,
+        GRAPH_Y,
+        PR,
+        Math.min(GRAPH_HEIGHT - PR, round((1 - value / max) * GRAPH_HEIGHT))
+      );
+
+      lastUpdate = performance.now();
     },
   };
 };
@@ -174,33 +150,34 @@ function createStats(game: Phaser.Game) {
   const style = stats.dom.style;
   style.display = 'flex';
   style.justifyContent = 'center';
-  style.gap = '2px';
+  style.gap = '6px';
+  style.cursor = '';
   style.right = '0';
   style.top = '';
   style.bottom = '10px';
   style.opacity = '0.7';
 
-  const renderPanel = stats.addPanel(new Stats.Panel('Render', '#ff8', '#221'));
-  const stepPanel = stats.addPanel(new Stats.Panel('Step', '#8ff', '#212'));
+  const fpsPanel = stats.addPanel(new Stats.Panel('FPS', '#9ad8e4', '#064b62'));
+  const framePanel = stats.addPanel(new Stats.Panel('Frame', '#f3b0c3', '#6b1e3d'));
+  const memoryPanel = stats.addPanel(new Stats.Panel('Memory', '#ffd59a', '#6b3e06'));
+  const renderPanel = stats.addPanel(new Stats.Panel('Render', '#e9f3a3', '#4c6b1a'));
+  const stepPanel = stats.addPanel(new Stats.Panel('Step', '#c3c3f3', '#1d1d6b'));
 
-  let step = 0;
-  let render = 0;
+  let last = 0;
+  let preStep = 0;
+  let preRender = 0;
 
-  game.events.on(Phaser.Core.Events.PRE_STEP, () => {
-    stats.begin();
-    step = performance.now();
-  });
+  game.events.on(Phaser.Core.Events.PRE_STEP, () => (preStep = performance.now()));
+  game.events.on(Phaser.Core.Events.POST_STEP, () => stepPanel.update(performance.now() - preStep));
+  game.events.on(Phaser.Core.Events.PRE_RENDER, () => (preRender = performance.now()));
 
-  game.events.on(Phaser.Core.Events.POST_STEP, () => {
-    stepPanel.update(performance.now() - step, 100);
-  });
-
-  game.events.on(Phaser.Core.Events.PRE_RENDER, () => {
-    render = performance.now();
-  });
   game.events.on(Phaser.Core.Events.POST_RENDER, () => {
-    renderPanel.update(performance.now() - render, 100);
-    stats.end();
+    renderPanel.update(performance.now() - preRender);
+    fpsPanel.update(1000 / game.loop.delta);
+    framePanel.update(performance.now() - preStep);
+    memoryPanel.update(performance.memory.usedJSHeapSize / 1048576);
+
+    last = performance.now();
   });
 }
 
