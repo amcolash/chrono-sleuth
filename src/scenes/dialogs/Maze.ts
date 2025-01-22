86efc1a2f6b25975a8d72e2e9d61f9d6ce57834d
Andrew McOlash <amcolash@gmail.com>
2025-01-21 22:27:49 -0800

    start work on maze tiles

--- src/scenes/dialogs/Maze.ts
+++ src/scenes/dialogs/Maze.ts
@@ -1,22 +1,23 @@
 import generateMaze, { Cell } from 'generate-maze';
-import { GameObjects, Geom, Math, Scene } from 'phaser';
+import { GameObjects, Geom, Input, Math as PhaserMath, Scene, Tilemaps } from 'phaser';
 
 import { InputManager, Key } from '../../classes/UI/InputManager';
 import { Config } from '../../config';
 import { MazeDialog } from './MazeDialog';
 
 const cells = 24;
-const cellSize = 48;
+const cellSize = 16;
 const stepTime = 140;
 
 export class Maze extends Scene {
   parent: MazeDialog;
   graphics: GameObjects.Graphics;
-
   keys: InputManager;
   maze: Cell[][];
   mazePlayer: GameObjects.Image;
-
+  tilemap: Tilemaps.Tilemap;
+  tileset: Tilemaps.Tileset;
+  layer: Tilemaps.TilemapLayer;
   nextUpdate: number;
 
   constructor() {
@@ -29,24 +30,42 @@ export class Maze extends Scene {
 
   preload() {
     this.load.setPath('assets');
-    this.load.image('maze_player', 'puzzles/maze_player.png');
+    this.load.image('maze_player', 'puzzles/maze/maze_player.png');
+    this.load.image('maze_tiles', 'puzzles/maze/maze_tiles.png');
   }
 
   create() {
     this.createMaze();
 
-    this.mazePlayer = this.add.image(0, 0, 'maze_player').setScale(0.5).setOrigin(-0.25);
+    this.mazePlayer = this.add
+      .image(0, 0, 'maze_player')
+      .setDisplaySize(cellSize / 2, cellSize / 2)
+      .setOrigin(-0.5);
 
-    this.cameras.main.startFollow(this.mazePlayer);
+    const camera = this.cameras.main;
+
+    camera.setBackgroundColor(0x4b692f);
+    camera.startFollow(this.mazePlayer);
+    camera.setZoom(4);
     this.keys = this.parent.keys;
 
-    this.parent.addTarget(this.graphics);
     this.parent.addTarget(this.mazePlayer);
+
+    this.input.on(
+      'wheel',
+      (
+        _pointer: Input.Pointer,
+        _currentlyOver: GameObjects.GameObject[],
+        _deltaX: number,
+        deltaY: number,
+        _deltaZ: number
+      ) => {
+        camera.zoom = Math.max(0.01, camera.zoom + deltaY * 0.0005);
+      }
+    );
   }
 
   getMazeSeed() {
-    // getClockRewind(this.parent.player.scene as Game);
-
     return this.parent.player.gameState.data.mazeSeed || 0;
   }
 
@@ -54,12 +73,45 @@ export class Maze extends Scene {
     const seed = this.getMazeSeed();
 
     this.maze = generateMaze(cells, cells, true, seed);
+
+    // Create tilemap and layer
+    const map = this.make.tilemap({
+      tileWidth: cellSize,
+      tileHeight: cellSize,
+      width: cells,
+      height: cells,
+    });
+
+    const tiles = map.addTilesetImage('maze_tiles');
+    map.createBlankLayer('layer', tiles!);
+
+    for (let y = 0; y < cells; y++) {
+      for (let x = 0; x < cells; x++) {
+        const cell = this.maze[y][x];
+
+        const top = cell.top ? 1 : 0;
+        const right = cell.right ? 1 : 0;
+        const bottom = cell.bottom ? 1 : 0;
+        const left = cell.left ? 1 : 0;
+
+        const tile = left + bottom * 2 + right * 4 + top * 8;
+
+        map.putTileAt(tile, x, y);
+      }
+    }
+
+    // this.drawGraphicsOld();
+
+    this.cameras.main.setViewport(50, 130, Config.width - 100, Config.height - 170);
+  }
+
+  drawGraphicsOld() {
     this.graphics = this.add.graphics();
 
     this.graphics.fillStyle(0x993322, 0.5);
     this.graphics.fillRect((cells - 1) * cellSize, (cells - 1) * cellSize, cellSize, cellSize);
 
-    this.graphics.lineStyle(3, 0x33aa33);
+    this.graphics.lineStyle(1, 0x33aa33);
 
     this.maze.forEach((row) => {
       row.forEach((col) => {
@@ -91,8 +143,6 @@ export class Maze extends Scene {
         }
       });
     });
-
-    this.cameras.main.setViewport(50, 130, Config.width - 100, Config.height - 170);
   }
 
   update(time: number, _delta: number): void {
@@ -100,19 +150,19 @@ export class Maze extends Scene {
 
     const keys = this.keys.keys;
 
-    const velocity = new Math.Vector2(0, 0);
+    const velocity = new PhaserMath.Vector2(0, 0);
     if (keys[Key.Left]) velocity.x = -cellSize;
     else if (keys[Key.Right]) velocity.x = cellSize;
     else if (keys[Key.Up]) velocity.y = -cellSize;
     else if (keys[Key.Down]) velocity.y = cellSize;
 
-    const newPosition = new Math.Vector2(this.mazePlayer.x + velocity.x, this.mazePlayer.y + velocity.y);
+    const newPosition = new PhaserMath.Vector2(this.mazePlayer.x + velocity.x, this.mazePlayer.y + velocity.y);
 
     if ((this.mazePlayer.x !== newPosition.x || this.mazePlayer.y !== newPosition.y) && this.canMove(newPosition)) {
       this.mazePlayer.setPosition(newPosition.x, newPosition.y);
 
-      const endPosition = new Math.Vector2((cells - 1) * cellSize, (cells - 1) * cellSize);
-      this.parent.setAngle(Math.Angle.BetweenPoints(newPosition, endPosition));
+      const endPosition = new PhaserMath.Vector2((cells - 1) * cellSize, (cells - 1) * cellSize);
+      this.parent.setAngle(PhaserMath.Angle.BetweenPoints(newPosition, endPosition));
 
       if (this.mazePlayer.x === endPosition.x && this.mazePlayer.y === endPosition.y) {
         this.parent.close(true);
@@ -122,19 +172,17 @@ export class Maze extends Scene {
     this.nextUpdate = time + stepTime;
   }
 
-  canMove(newPosition: Math.Vector2): boolean {
-    // return true;
-
+  canMove(newPosition: PhaserMath.Vector2): boolean {
     const newX = newPosition.x;
     const newY = newPosition.y;
 
-    const x = Math.FloorTo(newX / cellSize);
-    const y = Math.FloorTo(newY / cellSize);
+    const x = Math.floor(newX / cellSize);
+    const y = Math.floor(newY / cellSize);
 
     if (x < 0 || y < 0 || x >= cells || y >= cells) return false;
 
-    const currentX = Math.FloorTo(this.mazePlayer.x / cellSize);
-    const currentY = Math.FloorTo(this.mazePlayer.y / cellSize);
+    const currentX = Math.floor(this.mazePlayer.x / cellSize);
+    const currentY = Math.floor(this.mazePlayer.y / cellSize);
 
     const cell = this.maze[currentY][currentX];
 
