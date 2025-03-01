27e36f5755082a923c03f8ee6a6ce5f7a5e643b0
Andrew McOlash <amcolash@gmail.com>
2025-02-28 16:46:14 -0800

    start work on input type tracking

--- src/classes/UI/InputManager.ts
+++ src/classes/UI/InputManager.ts
@@ -10,12 +10,17 @@ export enum Key {
   Shift,
 }
 
+export type InputType = 'mouse' | 'keyboard' | 'gamepad';
+
 export class InputManager {
   scene: Scene;
 
   keys: Record<Key, boolean> = Object.keys(Key)
     .map(Number)
     .reduce((acc, key) => ({ ...acc, [key]: false }), {}) as Record<Key, boolean>;
+
+  /** Keep track of last input type. Use this to determine which icons to show in button prompt */
+  lastInput: InputType;
 
   constructor(scene: Scene) {
     this.scene = scene;
@@ -34,16 +39,29 @@ export class InputManager {
     this.listener(Key.Continue, 'ENTER');
     this.listener(Key.Back, 'BACKSPACE');
     this.listener(Key.Shift, 'SHIFT');
+
+    this.scene.input.on('pointerdown', () => {
+      this.lastInput = 'mouse';
+    });
+
+    const onPointerMove = () => (this.lastInput = 'mouse');
+
+    window.document.addEventListener('pointermove', onPointerMove);
+    scene.events.on('destroy', () => window.removeEventListener('pointermove', onPointerMove));
   }
 
   listener(key: Key, str: string, emitName?: string) {
-    this.scene.input.keyboard?.on(`keydown-${str}`, () => {
+    this.scene.input.keyboard?.on(`keydown-${str}`, (e: unknown) => {
       if (emitName) this.scene.input.keyboard?.emit(`keydown-${emitName}`);
       this.keys[key] = true;
+      if (e) this.lastInput = 'keyboard';
+      else this.lastInput = 'gamepad';
     });
-    this.scene.input.keyboard?.on(`keyup-${str}`, () => {
+    this.scene.input.keyboard?.on(`keyup-${str}`, (e: unknown) => {
       if (emitName) this.scene.input.keyboard?.emit(`keyup-${emitName}`);
       this.keys[key] = false;
+      if (e) this.lastInput = 'keyboard';
+      else this.lastInput = 'gamepad';
     });
   }
 
